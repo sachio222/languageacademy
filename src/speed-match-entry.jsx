b279e83a0a5a7873d311a6b9336b0d7a0da39c53
Jake Krajewski <brainpowerux@gmail.com>
2025-10-25 11:11:57 -0400

    speed-round

---
+++ src/speed-match-entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SpeedMatchDemo from './components/SpeedMatchDemo.jsx'
+import './styles/App.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <SpeedMatchDemo />
+)
+
